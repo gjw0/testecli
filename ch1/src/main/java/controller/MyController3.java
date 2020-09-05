4a3ecdd951ebd67bbb126b5e2baa3a44490cba25
zaixiakanke <gjw102938@163.com>
2020-09-05 18:40:16 +0800

    ecli0 提交mycontroller3

---
+++ ch1/src/main/java/controller/MyController3.java
@@ -0,0 +1,24 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController3 {
+	
+	
+	@RequestMapping("/kk")
+	public void kk() {
+		System.out.println("理论");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
