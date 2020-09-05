e2526b859c652725ffae17f399507424c75b9f7a
zaixiakanke <gjw102938@163.com>
2020-09-05 10:11:06 +0800

    首次提交ch1

---
+++ ch1/src/main/java/ch1app/AppConfig.java
@@ -0,0 +1,13 @@
+package ch1app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(basePackages = {"controller","dao"})
+public class AppConfig {
+	
+	
+	
+}
---
+++ ch1/src/main/java/controller/MyController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController {
+	
+	
+	@RequestMapping("/my")
+	public String my(Model m) {
+		
+		m.addAttribute("msg", "哈哈");
+		
+		return "my";
+		
+		
+	}
+	
+}
